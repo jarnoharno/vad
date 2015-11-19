0d2ef737867061c12de5bdf2a12cd12aa2d1f915
Pelkonen Ari M <apelkone@hp8x-48.cs.helsinki.fi>
2015-11-19 13:29:41 +0200

    New repo, file format examples, batch processing sketch

---
+++ vad-eval.py
@@ -0,0 +1,26 @@
+
+def main(signalcsv, noisecsv, snrcsv, algorithmscsv, samplerate, tmppath, resultcsv):
+    """ combine signals and noises with all SNRs and write audio output to tmppath with given samplerate"""
+    """ labels from signalcsv """
+    """ call algorithm from algorithm.csv"""
+    pass
+
+def predict(comb_files, labels, algos):
+    for alg in algos:
+        eval(alg) # g279
+
+def evaluate(predicted_path, truthpath, resultpath):
+    """ """
+
+def g279(labelpath):
+    """" call matlab g279.m """
+    """ store labels to csvfile"""
+    """ return ok/filename... """
+    pass
+
+def txt2list(path):
+    pass
+
+if __name__ == "__main__":
+    if len(argv == n):
+        main(argv*)
