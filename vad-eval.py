f959c41011242f12eeaf7e86c2cd40fcf3941fa3
Pelkonen Ari M <apelkone@hp8x-48.cs.helsinki.fi>
2015-11-20 15:28:50 +0200

    Audio file & signal tools

--- vad-eval.py
+++ vad-eval.py
@@ -1,12 +1,88 @@
+import csv
+from subprocess import call
+import librosa
+import numpy as np
+try:
+    try:
+        import scikits.audiolab as al
+    except ImportError:
+        import audiolab as al
+except ImportError:
+    al = None
+    print("Warning: scikits.audiolab not found! Using scipy.io.wavfile")
+    from scipy.io import wavfile
 
 def main(signalcsv, noisecsv, snrcsv, algorithmscsv, samplerate, tmppath, resultcsv):
-    """ combine signals and noises with all SNRs and write audio output to tmppath
-        with given samplerate
-        labels from signalcsv
-        write to tmppath/signalname_noisename_+-SNR.flac
-        call algorithm from algorithm.csv
-        which writes to predictiondir/algoname_signalname_noisename_+-SNR.txt
-        """
+    """
+    combine signals and noises with all SNRs and write audio output to tmppath
+    with given samplerate
+    labels from signalcsv
+    write to tmppath/signalname_noisename_+-SNR.flac
+    call algorithm from algorithm.csv
+    which writes to predictiondir/algoname_signalname_noisename_+-SNR.txt
+    """
+    signal_list = readcsv(signalcsv, True)
+    noise_list = readcsv(noisecsv)
+    snrlist = readcsv(snrcsv)
+    try:
+        snrlist = [float(x) for x in snrlist]
+    except ValueError:
+        print("Failed reading SNR definitions as float-values")
+    algorithms = readcsv(algorithmcsv, True)
+    combine(signal_list, noise_list, snrlist)
+
+def combine(signal_list, noise_list, snrlist, target_rate=8000):
+    for signal_file in signal_list:
+        signal, srate = read_soundfile(signal_file)
+        if srate != target_rate:
+            signal = librosa.core.resample(signal, srate, target_rate)
+        signal = rms_normalize(signal)
+        for noise_file in noise_list:
+            noise, nrate = read_soundfile(noise_file)
+            if nrate != target_rate:
+                noise = librosa.core.resample(noise, nrate, target_rate)
+            noise = rms_normalize(noise)
+            noisy_signal = signal*snrdb2ratio(signal))+noise
+            new_name = signal_file+"_"+noise_file
+            print("NOISE RMS:", noise_rms)
+
+def noise_tilify(noise, rate, sfade):
+    l = len(noise)/2
+    head = noise[:l]
+    tail = noise[l+1:]
+    fade = min(l, rate+sfade)
+
+def rms_normalize(signal):
+    return signal/rms(signal)
+
+def snrdb2ratio(db, signal, noise):
+    return 10**(db/10)*rms(signal)/rms(noise)
+
+def rms(signal):
+    return np.sqrt(np.mean(np.square(signal)))
+
+def peak(signal):
+    return np.max(np.abs(signal))
+
+def read_soundfile(filename):
+        soundfile = al.Sndfile(filename, 'r')
+        signal = soundfile.read_frames(soundfile.nframes)
+        if soundfile.channels == 1:
+            return signal, soundfile.samplerate
+        else:
+            return signal[:,0], soundfile.samplerate
+
+""" Read vad csv-files """
+def readcsv(filename, has_lists=False, delimiter=" "):
+    res = []
+    with open(filename, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=delimiter)
+        for row in reader:
+            if has_lists:
+                res.append(row[0])
+            else:
+                res.append(row)
+    return res
 
 def predict(comb_files, labels, algos):
     #["matlab", "g279.m" "combined_list.txt", prediction_dir]
@@ -24,9 +100,7 @@ def g279(combined_files, prediction_dir):
 def txt2list(path):
     pass
 
-def combine(signal, noise, snr):
-    pass
 
-if __name__ == "__main__":
-    if len(argv == n):
-        main(argv*)
+#if __name__ == "__main__":
+#    if len(argv == n):
+#        main(argv*)
